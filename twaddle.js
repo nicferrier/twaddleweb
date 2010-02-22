b2193e44f6c2da0b81a0109c2877276690a3f1a1
Nic Ferrier <nferrier@ferrier.me.uk>
2010-02-22 23:56:09 +0000

    get update working

--- twaddle.js
+++ twaddle.js
@@ -3,16 +3,17 @@
   
   a simple twitter client.
 
+
+  ---------------------------------
+  Copyright (C) 2010 by Nic Ferrier
  */
 var twaddle = new Object({
     "twaddler": function (fn, since) {
-        /**
-           Main function.
-           Calls twitter with jsonp to get the updates.
-           Can pass in 'since' if it's supplied.
-           When the updates arrive formats them as per the #tweettemplate
-           and then pass the resulting HTML to 'fn'
-         */
+        // Main function.
+        // Calls twitter with jsonp to get the updates.
+        // Can pass in 'since' if it's supplied.
+        // When the updates arrive formats them as per the #tweettemplate
+        // and then pass the resulting HTML to 'fn'
         var urlstr = "http://api.twitter.com/1/statuses/home_timeline.json?count=200&callback=?";
         if (since) {
             urlstr = "http://api.twitter.com/1/statuses/home_timeline.json?count=200&since_id" + since + "&callback=?";
@@ -40,31 +41,42 @@ var twaddle = new Object({
             });
     },
 
-    "refresh": function () {
-        /**
-           Completly initializes the tweet area.
-         */
+    "reload": function () {
+        // Completly initializes the tweet area.
         $("#tweets").empty();
         this.twaddler(function (h) {
             $("#tweets").append(h);
         });
     },
     
-    "update": function () {
-        /**
-           Updates the tweet area.
-           FIXME::: currently broken
-         */
+    "refresh": function () {
+        // Refreshes the tweet area with latest data
+        // FIXME::: currently broken
         this.twaddler(
             function (h) {  $("#tweets").before(h);},
             $("#tweets")[0].id
         );
+    },
+
+    "update": function () {
+        // Sends your update to twitter.
+        alert("updated!");
+        /*$.post(
+            "http://api.twitter.com/1/statuses/update.json?callback=?", 
+            $("#update").serialize(),
+            function (data, textStatus, request) {
+                alert("tweeted!!");
+            }
+        );*/
     }
 });
 
 $(document).ready(function () {
-    twaddle.refresh();
-    //setInterval("twaddle.update();", 5000);
+    twaddle.reload();
+    $("#tweetpostsink").load(function () {
+        $("textarea[name='status']").val("");
+    });
+    //setInterval("twaddle.refresh();", 5000);
 });
 
 /* end */
