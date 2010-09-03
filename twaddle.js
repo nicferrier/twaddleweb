f7dc44bde4c53186ba7decbacc32185d5ecd16da
nferrier <none@none>
2010-09-03 02:03:26 +0100

    switch to supertweet, make threads simpler

--- twaddle.js
+++ twaddle.js
@@ -46,14 +46,14 @@ var twaddle = new Object({
         // Can pass in 'since' if it's supplied.
         // When the updates arrive formats them as per the #tweettemplate
         // and then pass the resulting HTML to 'fn'
-        var urlstr = "http://api.twitter.com/1/statuses/home_timeline.json?count=200&callback=?";
+        var urlstr = "http://api.supertweet.net/1/statuses/home_timeline.json?count=200&callback=?";
         if (since) {
-            urlstr = "http://api.twitter.com/1/statuses/home_timeline.json?count=200&since_id=" + since + "&callback=?";
+            urlstr = "http://api.supertweet.net/1/statuses/home_timeline.json?count=200&since_id=" + since + "&callback=?";
         }
 
-        $.getJSON(
+        var x = $.getJSON(
             urlstr,
-            function (data) {
+            function (data, textStatus) {
                 $.each(data.reverse(), function(item, tweet) {
                     // Store the contact for later, including how many tweets
                     if (twaddle.contacts[tweet.user.screen_name] == undefined) {
@@ -75,7 +75,10 @@ var twaddle = new Object({
                         "created_at": tweet.created_at,
                         "reply_class": "hidden"
                     };
-                    if (tweet.in_reply_to_status_id != undefined) {
+                    // FIXME
+                    // would like to make proper thread list pulling in all refs to the thread
+                    if (tweet.in_reply_to_status_id != undefined
+                        && twaddle.contacts[tweet.in_reply_to_screen_name] != undefined) {
                         var id = tweet.in_reply_to_status_id;
                         var screen_name = tweet.in_reply_to_screen_name;
                         attribs["reply_class"] = "";
@@ -90,9 +93,6 @@ var twaddle = new Object({
                             attribs["reply_created_at"] = $('#' + id + " div.date").text();
                             attribs["reply_html"] = $('#' + id + " span.text").html();
                         }
-                        
-                        // Now delete the original reply
-                        $("#" + id).remove();
                     }
                     fn(attribs, tweet);
                 });
